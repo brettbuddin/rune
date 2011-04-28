a06ce1e99edbaae9dc0007b00632c26204c9e94f
Brett Buddin <brett@intraspirit.net>
2011-04-28 12:47:15 -0400

    Fixing the algorithm

--- lib/rune.rb
+++ lib/rune.rb
@@ -14,50 +14,44 @@ class Rune
     @params = params
   end
   
-  # Generate the signature
+  # Generate the signature.
   # 
-  # Returns the String signature
+  # Returns the String signature.
   def generate
-    data = recursive_flatten(recursive_stringify_keys(@params)).to_a.sort_by{|k| k[0].to_s}.flatten.join
+    data = flatten(stringify_keys(@params)).join
     digest = OpenSSL::Digest::Digest.new("sha1")
     Base64.encode64(OpenSSL::HMAC.digest(digest, @auth_token, "#{@url}#{data}")).strip
   end
 
   private
 
-  # Stringify the keys of a Hash
+  # Stringify the keys of a Hash.
   # 
-  # Returns the Hash that has strings for keys
+  # Returns the Hash that has strings for keys.
   def stringify_keys(hash)
     hash.keys.each do |key|
       hash[key.to_s] = hash.delete(key)
     end
-    hash
-  end
 
-  # Recursively apply stringify_keys to a Hash
-  # 
-  # Returns the Hash that has strings for keys
-  def recursive_stringify_keys(hash)
-    hash = stringify_keys(hash)
     hash.values.select{ |v| v.is_a?(Hash) }.each do |h| 
-      recursive_stringify_keys(h)
+      stringify_keys(h)
     end
     hash
   end
   
-  # Recursively flatten the hash
+  # Recursively flatten the hash. Each level of the hash is sorted
+  # alphabetically based on key name.
   #
-  # Returns an Array of the hash sorted alphabetically. Mind fuck.
-  def recursive_flatten(hash)
-    (recursive = lambda do |v|
-      if v.is_a?(Array)
-        v.flatten.map{ |v| recursive.call(v) }
-      elsif v.is_a?(Hash)
-        v.to_a.sort_by{|k| k[0].to_s}.map { |v| recursive.call(v) }.flatten
-      else
-        v.to_s
-      end
-    end).call(hash)
+  # Returns an Array of the hash, flattened. Mind fuck.
+  def flatten(value)
+    if value.is_a?(Array)
+      value.flatten.map{ |v| flatten(v) }
+    elsif value.is_a?(Hash)
+      value.to_a.sort_by do |k| 
+        k[0].to_s
+      end.map { |v| flatten(v) }.flatten
+    else
+      value.to_s
+    end
   end
 end
--- spec/rune_spec.rb
+++ spec/rune_spec.rb
@@ -7,21 +7,36 @@ describe Rune do
       'test',
       {:person => {:name  => "Name", :age => "29"}, :other => "dude"}
     )
-    rune.generate.should == 'iPMlDY50Z5SYdj6+ISUUtSYxbZQ='
+    rune.generate.should == 'qiPvkhsTdXJ/2Ee12UrSSfmIRYI='
   end
 
   it "should generate the same signature for hashes with symbols and string keys" do
     rune_with_symbols = Rune.new(
       'http://localhost/people',
       'test',
-      {:person => {:name  => "Name", :age => "29"}}
+      {:person => {:name  => "Name", :age => "29", :crazy => {:nested => "stuff"}}}
     )
     rune_with_strings = Rune.new(
       'http://localhost/people',
       'test',
-      {'person' => {'name'  => "Name", 'age' => "29"}}
+      {'person' => {'name'  => "Name", 'age' => "29", 'crazy' => {'nested' => "stuff"}}}
     )
 
     rune_with_symbols.generate.should == rune_with_symbols.generate
+  end
+
+  it "should generate the same signature for two orderings of parameters" do
+    rune = Rune.new(
+      'http://localhost/people',
+      'test',
+      {:other => "dude", :person => {:age => "29", :name  => "Name"}}
+    )
+    rune2 = Rune.new(
+      'http://localhost/people',
+      'test',
+      {"person" => {:name  => "Name", :age => "29"}, :other => "dude"}
+    )
+
+    rune.generate.should == rune2.generate
   end
 end
